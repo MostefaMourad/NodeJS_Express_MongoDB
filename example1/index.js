ff24edb983d23010b3bca01bf4200ec2bfa6e3c6
MostefaMourad <hm_mostefa@esi.dz>
2020-11-03 10:10:41 +0100

    Callbacks and errors

--- example1/index.js
+++ example1/index.js
@@ -2,14 +2,19 @@ var rect = require('./rectangle');
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + "And b = "+b);
-    if((l<=0) || (b<=0)){
-        console.log("Rectangle dimension should be >= 0");
-    }
-    else{
-        console.log("The area of the rectangle is "+ rect.area(l,b));
-        console.log("The area of the rectangle is "+ rect.perimeter(l,b));
-    }
+    rect(l,b,(err,rectangle)=> {
+        if(err){
+            console.log("ERROR : ",err.message)
+        }else{
+            console.log("The Area of the rect ("+l+","+b+") = "+rectangle.area());
+            console.log("The Perimeter of the rect ("+l+","+b+") = "+rectangle.perimeter());
+        }
+    });
+    console.log("This statement was after the call to rect()");
 }
 
 solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
 
