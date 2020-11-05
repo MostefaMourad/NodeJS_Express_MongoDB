698b18124467c2ae4d382ca96d2c44bd50ab737c
MostefaMourad <hm_mostefa@esi.dz>
2020-11-05 08:47:37 +0100

    CRUD for leaders

--- Assignment1/index.js
+++ Assignment1/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const http = require('http');
 const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
 const promotionRouter = require('./routes/promotionRouter');
-
 
 const port = 3000;
 const hostname = "localhost";
@@ -10,6 +10,7 @@ const hostname = "localhost";
 const app = express();
 app.use('/dishes',dishRouter);
 app.use('/promotions',promotionRouter);
+app.use('/leaders',leaderRouter);
 
 const server = http.createServer(app);
 
