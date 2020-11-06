106fdc1308f05d06a553ecff96da0492882dcdd9
MostefaMourad <hm_mostefa@esi.dz>
2020-11-06 13:26:52 +0100

    Promotion model creation

---
+++ Assignment2/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const http = require('http');
+const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promotionRouter = require('./routes/promoRouter');
+
+const port = 3000;
+const hostname = "localhost";
+
+const app = express();
+app.use('/dishes',dishRouter);
+app.use('/promotions',promotionRouter);
+app.use('/leaders',leaderRouter);
+
+const server = http.createServer(app);
+
+server.listen(port,hostname,() => {
+    console.log(`Server is running at http://${hostname}:${port}`);
+});
+
