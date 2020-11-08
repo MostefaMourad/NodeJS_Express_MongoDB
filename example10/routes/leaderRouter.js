61efbfa86931e7fb5c24db4f023a3682ba80df38
MostefaMourad <hm_mostefa@esi.dz>
2020-11-08 12:11:07 +0100

    Passport with Jwt

---
+++ example10/routes/leaderRouter.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Leaders = require('../models/leaders');
+const authenticate = require('../authenticate');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.get((req,res,next) => {
+    Leaders.find({})
+    .then( (leaders) => {
+        res.statusCode = 200 ;
+        res.setHeader('Content-Type','application/json');
+        res.json(leaders);
+    },(err) => next(err))
+    .catch((err) => next(err));
+})
+.post(authenticate.verifyUser,(req, res, next) => {
+    Leaders.create(req.body)
+    .then( (leader) => {
+        res.statusCode = 201 ;
+        res.setHeader('Content-Type','application/json');
+        res.json(leader);
+    },(err) => next(err))
+    .catch((err) => next(err));
+})
+.put(authenticate.verifyUser,(req, res, next) => {
+    res.statusCode = 403;
+    err = new Error('PUT operation not supported on /Leaders');
+    next(err);
+})
+.delete(authenticate.verifyUser,(req, res, next) => {
+    Leaders.deleteMany({})
+    .then( (resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','json/application');
+        res.json(resp);
+    },(err) => next(err))
+    .catch((err) => next(err));
+});
+
+leaderRouter.route('/:leaderId')
+.get((req,res,next) => {
+    Leaders.findById(req.params.leaderId)
+    .then( (leader) => {
+        res.statusCode = 200 ;
+        res.setHeader('Content-Type','application/json');
+        res.json(leader);
+    },(err) => next(err))
+    .catch((err)=>next(err));
+})
+.post(authenticate.verifyUser,(req, res, next) => {
+    res.statusCode = 403;
+    err = new Error('POST operation not supported on /Leaders/'+req.params.leaderId);
+    next(err);
+})
+.put(authenticate.verifyUser,(req, res, next) => {
+    Leaders.findByIdAndUpdate(req.params.leaderId,
+        {
+            $set:req.body
+        },{new:true})
+        .then((leader) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type','json/application');
+            res.json(leader);
+        },(err) => next(err))
+        .catch((err) => next(err));
+})
+.delete(authenticate.verifyUser,(req, res, next) => {
+    Leaders.findByIdAndDelete(req.params.leaderId)
+    .then((leader) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type','json/application');
+        res.json(leader);
+    },(err)=>next(err))
+    .catch((err)=>next(err));
+});
+
+
+module.exports = leaderRouter;
---
+++ example10/routes/users.js
@@ -0,0 +1,50 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('../models/user');
+var router = express.Router();
+router.use(bodyParser.json());
+var passport = require('passport');
+var authenticate = require('../authenticate');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signup',function(req,res,next) {
+  User.register(new User({username:req.body.username}),req.body.password,
+  (err,user) => {
+    if(err){
+      res.statusCode = 500;
+      next(err); 
+    }else{
+      passport.authenticate('local')(req,res,() => {
+        res.statusCode = 201 ;
+        res.setHeader('Content-Type','application/json');
+        res.json({sucess:true,status:'Registration sucessfull !'})
+      });
+    }
+  });
+})
+
+router.post('/login',passport.authenticate('local'),(req,res) => {
+    var token = authenticate.getToken({_id:req.user._id}); 
+    res.statusCode = 200 ;
+    res.setHeader('Content-Type','application/json');
+    res.json({sucess:true,token:token,status:'You are logged in'});
+});
+
+router.get('/logout',(req,res) => {
+  if(req.session){
+    req.session.destroy();
+    res.clearCookie('session-id');
+    res.redirect('/');
+  }
+  else{
+    err = new Error('YOu are not logged in ');
+    res.statusCode = 403 ;
+    next(err);
+  }
+});
+
+module.exports = router;
