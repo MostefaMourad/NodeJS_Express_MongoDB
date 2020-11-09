6362fdebbd4b5c9ab3959bca1cade5c141a43172
MostefaMourad <hm_mostefa@esi.dz>
2020-11-09 05:43:45 +0100

    users API

--- Assignment3/routes/users.js
+++ Assignment3/routes/users.js
@@ -7,8 +7,14 @@ var passport = require('passport');
 var authenticate = require('../middleware/authenticate');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', authenticate.verifyUser,authenticate.verifyAdmin,function(req, res, next) {
+  User.find({})
+  .then((users) => {
+    res.statusCode = 200 ;
+    res.setHeader('Content-Type','json/application');
+    res.json(users);
+  })
+  .catch((err) => next(err));
 });
 
 router.post('/signup', (req,res,next) => {
