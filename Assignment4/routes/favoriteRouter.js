2c2de116d9c966dc834befb574d3f4c8fc8c8ff6
MostefaMourad <hm_mostefa@esi.dz>
2020-11-11 12:07:23 +0100

    Implementing /favorites/dishId

--- Assignment4/routes/favoriteRouter.js
+++ Assignment4/routes/favoriteRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Favorite = require('../models/favorite');
+const Dish = require('../models/dishes');
 const bodyParser = require('body-parser');
 const authenticate = require('../middleware/authenticate');
 
@@ -81,11 +82,81 @@ favoriteRouter.route('/')
 });
 
 favoriteRouter.route('/:dishId')
-.all()
-.get()
-.post()
-.put()
-.delete();
+.all(authenticate.verifyUser,(req,res,next) => {
+    next();
+})
+.get((req,res,next) => {
+    res.statusCode = 403 ;
+    err = new Error('GET operation not supported on /favorites/'+req.params.dishId);
+    next(err);
+})
+.post((req,res,next) => {
+    Dish.findById(req.params.dishId) // Check if the dish exists in the database
+    .then((dish) =>{
+        if(dish){
+            Favorite.findOne({user:req.user._id}) // check if the favorite document exists
+            .then((favorite) => {
+                if(favorite){
+                    if((favorite.dishes).indexOf(req.params.dishId) < 0){
+                        (favorite.dishes).push(req.params.dishId);
+                    }   
+                }else{
+                    favorite = new Favorite({user:req.user._id,dishes:[]});
+                    (favorite.dishes).push(req.params.dishId);
+                }
+                favorite.save().then(() => {
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type','application/json');
+                    res.json(favorite);
+                });
+            })
+            .catch((err) => next(err));
+        }else{
+            res.statusCode = 403 ;
+            err = new Error('The Dish does not exist');
+            next(err);
+        }
+    })
+    .catch((err) => next(err));  
+})
+.put((req,res,next) => {
+    res.statusCode = 403 ;
+    err = new Error('PUT operation not supported on /favorites/'+req.params.dishId);
+    next(err); 
+})
+.delete((req,res,next) => {
+    Dish.findById(req.params.dishId) // Check if the dish exists in the database
+    .then((dish) =>{
+        if(dish){
+            Favorite.findOne({user:req.user._id}) // check if the favorite document exists
+            .then((favorite) => {
+                if(favorite){
+                    if((favorite.dishes).indexOf(req.params.dishId) < 0){
+                        res.statusCode = 403 ;
+                        err = new Error("The Dish isn't in your list of favourite dishes");
+                        next(err);
+                    }else{
+                        favorite.dishes = (favorite.dishes).filter((item) => (favorite.dishes).indexOf(item) !== (favorite.dishes).indexOf(req.params.dishId)); 
+                        favorite.save().then(() => {
+                            res.statusCode = 200;
+                            res.setHeader('Content-Type','application/json');
+                            res.json(favorite);
+                        });
+                    }   
+                }else{
+                    res.statusCode = 403 ;
+                    err = new Error("You have no favourite dishes");
+                    next(err);
+                }
+            })
+            .catch((err) => next(err));
+        }else{
+            res.statusCode = 403 ;
+            err = new Error('The Dish does not exist');
+            next(err);
+        }
+    })
+    .catch((err) => next(err));
+});
 
 module.exports = favoriteRouter ;
-
